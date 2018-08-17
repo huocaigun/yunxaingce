b996466854c3b9c26a831d26eb7051c4de9b389d
wangyunfei <wangyunfei@wangyunfei>
2018-08-18 07:45:56 +0800

    yunxaingce

---
+++ src/main/java/com/bw/QianrushiRk9Application.java
@@ -0,0 +1,12 @@
+package com.bw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QianrushiRk9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QianrushiRk9Application.class, args);
+	}
+}
---
+++ src/main/java/com/bw/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.bw;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(QianrushiRk9Application.class);
+	}
+
+}
